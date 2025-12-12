ca480f0b9ade78d1ce7b29647b504f2d7104866c
Shifa <bsse1301@iit.du.ac.bd>
2025-12-12 20:23:44 +0600

    refactor: remove duplicate codes

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -8,19 +8,11 @@ def check_password_strength() -> str:
     base_strength = check_password_length(password, min_length, max_length)
     diversity = calculate_diversity_score(password)
     
-    if base_strength == INVALID:
-        return INVALID
-    if base_strength == WEAK:
-        return WEAK
-    if base_strength == MEDIUM:
-        if diversity >= DIVERSITY_SCORE_THREE:
-            return STRONG
-        else:
-            return MEDIUM
-    if base_strength == STRONG:
-        if diversity >= DIVERSITY_SCORE_THREE:
-            return STRONG
-        else:
-            return MEDIUM    
+    if base_strength in (INVALID, WEAK):
+        return base_strength
+    elif diversity >= DIVERSITY_SCORE_THREE:
+        return STRONG
+    else:    
+        return MEDIUM    
         
 print(check_password_strength())
